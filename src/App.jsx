30e2f714afb87b09e164185eb5d66489bbc8c019
Douglas Matos <matos.douglas.17@gmail.com>
2024-07-12 12:24:09 -0300

    inserção de logica do get do QrCode da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import './main.css'
 import './app.css'
 
-function App() {
+//import componentes
+import { QrCodeComponents } from './Componentes/QrCode.jsx'
+
+export function App() {
 
   return (
     <>
       <section className='main-body'>
         <div className='content'>
-          <h1>Olá Mundo!</h1>
+            <div>
+              <h2>Como conectar</h2>
+                <p>1. Abra o aplicativo do WhatsApp</p>
+                <p>2. Busque as opções, geralmente três pontinhos na parte superior direita do aplicativo.</p>
+                <p>3. Nas opções terá &quot;Dispositivos conectados&quot;, clique nela.</p>
+                <p>4. Clique em &quot;Conectar Dispositivo&quot; e aponte a câmera para o QRcode abaixo</p>
+            </div>
+          <QrCodeComponents/>
         </div>
       </section>
       
     </>
   )
 }
-
-export default App
---
+++ src/Componentes/QrCode.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+
+import QRCode from 'qrcode';
+
+export function QrCodeComponents() {
+  async function fetchQRCode() {
+    try {
+      const response = await fetch('https://whatsapp-bot-production-b594.up.railway.app/qr');
+      const qrCodeData = await response.text();
+      const qrcodeElement = document.getElementById('qrcode');
+      QRCode.toCanvas(qrcodeElement, qrCodeData, {
+        width: 350,
+        color: {
+          dark: '#043d36',
+          light: '#dcf8c6'
+        }
+      }, (error) => {
+        if (error) console.error('Erro ao gerar o QR code:', error);
+      });
+    } catch (error) {
+      console.error('Erro ao buscar o QR code:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchQRCode();
+  }, []);
+
+  return (
+    <>
+      <canvas id="qrcode">Ola mundo!</canvas>
+    </>
+  );
+}
